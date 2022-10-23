d542dbda1d43ba07632950575365cb7aa3899981
Halderpritam123 <halderpritamhalder1@gmail.com>
2022-10-23 04:34:43 -0700

    edited

--- palidromeStr.js
+++ palidromeStr.js
@@ -3,7 +3,7 @@ for(let i=str.length-1;i>=0;i--){
     new_str+=str[i];
 }
 if(new_str===str){
-console.log();
+console.log("is a palindrome string");
 }else{
 console.log("not a palindrome string");
 }
