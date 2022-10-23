4d7b1aae731df3bea7265130b003810c92fe17be
Halderpritam123 <halderpritamhalder1@gmail.com>
2022-10-22 22:26:43 -0700

    chack a no is prime or not

---
+++ chackPrime.js
@@ -0,0 +1,21 @@
+let count=0;
+
+  for(let i=1;i<=num;i++){
+ 
+   if(num%i==0){
+ 
+     count++;
+   
+    }
+ 
+   }
+ 
+ if(count==2){
+  
+  console.log("");
+ 
+   }else{
+  
+  console.log("non prime");
+ 
+   }
